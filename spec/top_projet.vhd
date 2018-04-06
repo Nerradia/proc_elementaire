9889fe9eb33a3f1f14c517af8e2f693dfdc2ccbd
Unknown <nerradia@gmail.com>
2018-04-06 14:33:53 +0200

    Utilisation d'une RAM Xilinx

--- spec/top_projet.vhd
+++ spec/top_projet.vhd
@@ -55,6 +55,7 @@ architecture rtl of top_projet is
       );
   end component;
 
+/*
   component RAM is
     generic (
       data_size    : integer := 8; -- Taille de chaque mot stocké
@@ -70,6 +71,7 @@ architecture rtl of top_projet is
       data_out : out std_logic_vector (data_size -1 downto 0)
       );
   end component;
+*/
 
   component top_prog is 
     generic (
@@ -96,15 +98,29 @@ architecture rtl of top_projet is
     );
   end component;
 
-  signal bus_en_mem : std_logic;
-  signal bus_R_W        : std_logic;
-  signal bus_address    : std_logic_vector (address_size-1 downto 0);
-  signal bus_data_out   : std_logic_vector (data_size -1 downto 0);
-  signal bus_data_in    : std_logic_vector (data_size -1 downto 0);
-  signal cpu_init       : std_logic;
+
+  component blk_mem_gen_0 is
+    Port ( 
+      clka      : in STD_LOGIC;
+      ena       : in STD_LOGIC;
+      wea       : in STD_LOGIC_VECTOR ( 0 to 0 );
+      addra     : in STD_LOGIC_VECTOR ( 5 downto 0 );
+      dina      : in STD_LOGIC_VECTOR ( 7 downto 0 );
+      douta     : out STD_LOGIC_VECTOR ( 7 downto 0 )
+    );
+  end component;
+
+
+  signal cpu_bus_en_mem     : std_logic;
+  signal cpu_bus_R_W        : std_logic;
+  signal cpu_bus_address    : std_logic_vector (address_size-1 downto 0);
+  signal cpu_bus_data_out   : std_logic_vector (data_size -1 downto 0);
+  signal cpu_bus_data_in    : std_logic_vector (data_size -1 downto 0);
+  signal cpu_init           : std_logic;
 
 begin
 
+/*
 inst_RAM : RAM
     generic map(
         data_size    => data_size,
@@ -114,12 +130,22 @@ inst_RAM : RAM
       clk      => clk,
 
       -- Bus
-      clk_en   => bus_en_mem,
-      R_W      => bus_R_W,
-      address  => bus_address,
-      data_in  => bus_data_out,  -- Ici on croise les signaux, (data out du bus = sortie du maitre, donc entrée de la RAM)
-      data_out => bus_data_in
-    );
+      clk_en   => cpu_bus_en_mem,
+      R_W      => cpu_bus_R_W,
+      address  => cpu_bus_address,
+      data_in  => cpu_bus_data_out,  -- Ici on croise les signaux, (data out du bus = sortie du maitre, donc entrée de la RAM)
+      data_out => cpu_bus_data_in
+    );*/
+
+int_ram_cpu : blk_mem_gen_0
+  port map ( 
+    clka     => clk,
+    ena      => cpu_bus_en_mem,
+    wea      => (0 => cpu_bus_R_W),
+    addra    => cpu_bus_address,
+    dina     => cpu_bus_data_out,
+    douta    => cpu_bus_data_in
+  );
 
 inst_CPU : top_CPU
     generic map(
@@ -136,14 +162,14 @@ inst_CPU : top_CPU
       cpu_init     => cpu_init, --cpu_rst,      -- reset synchrone qui vient du programmeur
 
       -- Bus
-      bus_en_mem       => bus_en_mem,
-      bus_R_W          => bus_R_W,
-      bus_address      => bus_address,
-      bus_data_in      => bus_data_in,
-      bus_data_out     => bus_data_out
+      bus_en_mem       => cpu_bus_en_mem,
+      bus_R_W          => cpu_bus_R_W,
+      bus_address      => cpu_bus_address,
+      bus_data_in      => cpu_bus_data_in,
+      bus_data_out     => cpu_bus_data_out
     );
 
-inst_top_prog : top_prog
+inst_top_prog_cpu : top_prog
     generic map (
       data_size    => data_size,
       address_size => address_size,
@@ -159,16 +185,15 @@ inst_top_prog : top_prog
       prog_status_led => prog_status_led,
       cpu_init        => cpu_init,
 
-      bus_en_mem      => bus_en_mem,
-      bus_R_W         => bus_R_W,
-      bus_data_out    => bus_data_out,
-      bus_address     => bus_address,
-      led_debug       => led_debug
+      bus_en_mem      => cpu_bus_en_mem,
+      bus_R_W         => cpu_bus_R_W,
+      bus_data_out    => cpu_bus_data_out,
+      bus_address     => cpu_bus_address
     );
 
 
-  addr         <= bus_address;
-  data_mem_in  <= bus_data_out;
-  data_mem_out <= bus_data_in;
+  addr         <= cpu_bus_address;
+  data_mem_in  <= cpu_bus_data_out;
+  data_mem_out <= cpu_bus_data_in;
 
 end rtl;
